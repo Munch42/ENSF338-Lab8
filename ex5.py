cf905507147c874af6e04245b6ddcf444cedb1d7
ade123a <bashirniyi@gmail.com>
2025-03-24 12:11:58 -0600

    Created ex5. Done part 1

---
+++ ex5.py
@@ -0,0 +1,4 @@
+#1 Topological sorting can be implemented using Depth First Search
+# You keep searching until you find a leaf node, then pop it of the graph. 
+#This is done recursively, so at the end of it, you get the result backwards, 
+#which can then be reversed.
